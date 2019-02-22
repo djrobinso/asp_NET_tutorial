c9a333b45103b0165b33ed535fb47ad508e5a54c
Deyonta <deyontajanardrobinson@gmail.com>
2019-02-22 16:54:12 -0500

    Add project files.

---
+++ RazorPagesMovie/Pages/Movies/Index.cshtml.cs
@@ -0,0 +1,45 @@
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using RazorPagesMovie.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace RazorPagesMovie.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+
+        private readonly RazorPagesMovie.Models.MovieContext _context;
+
+        public IndexModel(RazorPagesMovie.Models.MovieContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Movie> Movie { get; set; }
+        public SelectList Genres { get; set; }
+        public string MovieGenre { get; set; }
+
+        public async Task OnGetAsync(string searchString)
+        {
+            var movies = from m in _context.Movie
+                         select m;
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                movies = movies.Where(s => s.Title.Contains(searchString));
+            }
+
+            Movie = await movies.ToListAsync();
+        }
+    }
+}
